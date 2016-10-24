18c51555bf1e59e52f62e063dbf8de3f22d4f1e4
Chris Konstad <chriskon149@gmail.com>
2016-10-24 14:36:17 -0700

    [Request] Can now filter based on location, and can get distance between locations

--- server/app/models/request.rb
+++ server/app/models/request.rb
@@ -4,4 +4,26 @@ class Request < ActiveRecord::Base
 
   validates :title, :user, :amount, :lat, :long, :due, presence: true
   validates :amount, :numericality => { :greater_than_or_equal_to => 0 }
+
+  def Request.distance(longA, latA, longB, latB)
+    deg2Rad = lambda { |d| d * (Math::PI/180.0) }
+    earth_radius = 3961.0
+    dlon = deg2Rad.call(longB - longA)
+    dlat = deg2Rad.call(latB - latA)
+    a = (Math.sin(dlat/2.0)**2.0) +
+      Math.cos(deg2Rad.call(latA)) *
+      Math.cos(deg2Rad.call(latB)) *
+      (Math.sin(dlon/2.0)**2.0)
+    c = 2.0 * Math.atan2(Math.sqrt(a), Math.sqrt(1.0 - a))
+    return (earth_radius * c).abs
+  end
+
+  def Request.openNear(longitude, latitude, radius_miles)
+    # Find all non-completed requests within radius_miles of longitude/latitude
+    return Request.includes(:trans)
+      .where(:transactions => { :request_id => nil })
+      .select { |r|
+      Request.distance(longitude, latitude, r.long, r.lat) <= radius_miles
+    }
+  end
 end
--- server/test/models/request_test.rb
+++ server/test/models/request_test.rb
@@ -12,4 +12,30 @@ class RequestTest < ActiveSupport::TestCase
     assert_equal testuser.id, req.trans.payee.id
     assert_equal namey.id, req.trans.payer.id
   end
+
+  test "distance" do
+    longA = -77.037852
+    latA = 38.898556
+    longB = -77.043934
+    latB = 38.897147
+
+    assert_equal 0.341, Request.distance(longA, latA, longB, latB).round(3)
+  end
+
+  test "check near" do
+    completed = requests(:samplecompleted)
+    open = requests(:sampleopen)
+
+    # Ensure they take place at the sample place
+    # and that one is open and one is closed
+    assert_equal completed.long, open.long
+    assert_equal completed.lat, open.lat
+    assert_not_nil completed.trans
+    assert_nil open.trans
+
+    requests = Request.openNear(1.51, 1.51, 5.0)
+
+    assert requests.include? open
+    assert_not requests.include? completed
+  end
 end
