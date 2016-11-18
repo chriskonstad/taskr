325580eb36f69c8ea35eb72e64cb7089f5602be6
Chris Konstad <chriskon149@gmail.com>
2016-11-17 16:11:39 -0800

    [Client] Moved profile over to rating bar

--- client/taskrclient/app/src/main/java/com/taskr/client/ProfileFragment.java
+++ client/taskrclient/app/src/main/java/com/taskr/client/ProfileFragment.java
@@ -1,33 +1,24 @@
 package com.taskr.client;
 
 import android.app.ProgressDialog;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RatingBar;
-import android.widget.TextView;
 
 import com.koushikdutta.ion.Ion;
 import com.taskr.api.Api;
 import com.taskr.api.Profile;
 import com.taskr.api.Rating;
-import com.taskr.api.Request;
 
 import java.util.ArrayList;
-import java.util.List;
 
-import butterknife.BindString;
 import butterknife.BindView;
-import butterknife.BindViews;
 import butterknife.ButterKnife;
 
 /**
@@ -39,28 +30,7 @@ public class ProfileFragment extends Fragment {
     public static final String UID = "user_id";
     @BindView (R.id.profile_picture) ImageView profilePicture;
     @BindView (R.id.ratings_list) ListView ratingsList;
-    //@BindView (R.id.rating) RatingBar ratingBar;
-    @BindViews({R.id.star1,
-            R.id.star2,
-            R.id.star3,
-            R.id.star4,
-            R.id.star5})
-    List<ImageView> stars;
-    static final ButterKnife.Setter<ImageView, Double> SET_STARS =
-            new ButterKnife.Setter<ImageView, Double>() {
-        @Override
-        public void set(@NonNull ImageView view, Double value, int index) {
-            int starNum = index + 1;
-            double diff = value - starNum;
-            if(-0.25 <= diff) {
-                view.setImageResource(R.drawable.star);
-            } else if (-0.75 <= diff) {
-                view.setImageResource(R.drawable.star_half);
-            } else {
-                view.setImageResource(R.drawable.star_outline);
-            }
-        }
-    };
+    @BindView (R.id.overall_rating) RatingBar ratingBar;
 
     public ProfileFragment() {
         // Required for fragment subclass
@@ -95,8 +65,7 @@ public class ProfileFragment extends Fragment {
                 dialog.dismiss();
                 getActivity().setTitle(profile.name);
                 Log.i(TAG, "Avg rating: " + Double.toString(profile.avgRating));
-                ButterKnife.apply(stars, SET_STARS, profile.avgRating);
-                //ratingBar.setRating((float)profile.avgRating);
+                ratingBar.setRating((float)profile.avgRating);
                 Ion.with(profilePicture)
                         .placeholder(R.drawable.loadingpng)
                         .load(profile.getProfilePictureUrl());
