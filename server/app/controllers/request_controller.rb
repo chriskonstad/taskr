38effc00f47824bc7e81ff4bc5ce9d8ae6eee8d7
guilam34 <guillaumelam34@yahoo.com>
2016-11-24 11:47:04 -0800

    nvm

--- server/app/controllers/request_controller.rb
+++ server/app/controllers/request_controller.rb
@@ -97,12 +97,11 @@ class RequestController < ApplicationController
   def notify(data, collapse_key = nil)
     fcm = FCM.new("AIzaSyAfgwTlcsudSPq5xh2BVCFcQ8I4z9j3nq8")
     @dev = Device.search(params[:user_id])
-    registration_id = [@dev.registration_id]
     options = {
       data: data,
       collapse_key: collapse_key || 'my_app'
     }
-    response = fcm.send(registration_id, options)
+    response = fcm.send(@dev.registration_id, options)
 
   end
 
